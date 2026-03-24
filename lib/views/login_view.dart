99a47f2585806759dd7ac5358700c62c1c3349af
0p3r4d0r <anderson.valenzuela@udea.edu.co>
2026-03-24 18:02:01 -0500

    ajustes vistas login

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -9,15 +9,42 @@ class LoginView extends StatelessWidget {
     final vm = context.watch<LoginViewModel>();
 
     return Scaffold(
-      appBar: AppBar(title: Text('Login MVVM')),
+      appBar: AppBar(
+        title: Text('REDIME'),
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          color: Colors.black,
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
+            const SizedBox(height: 90),
+            const Text(
+              'Inicar sesión',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              textAlign: TextAlign.center,
+              'Ingresa tu correo electronico y contraseña para iniciar tu sesión',
+              style: TextStyle(fontSize: 17, fontWeight: FontWeight.normal),
+            ),
+
             // TextField para email
             TextField(
-              decoration: InputDecoration(labelText: 'Email'),
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 8,
+                ),
+              ),
+
               onChanged: vm.setEmail,
             ),
 
@@ -25,7 +52,14 @@ class LoginView extends StatelessWidget {
 
             // TextField para password
             TextField(
-              decoration: InputDecoration(labelText: 'Password'),
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 8,
+                ),
+              ),
               obscureText: true,
               onChanged: vm.setPassword,
             ),
@@ -38,6 +72,14 @@ class LoginView extends StatelessWidget {
                 : ElevatedButton(
                     onPressed: () => vm.login(),
                     child: Text('Login'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                      minimumSize: Size(double.infinity, 40),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(7),
+                      ),
+                    ),
                   ),
 
             SizedBox(height: 24),
