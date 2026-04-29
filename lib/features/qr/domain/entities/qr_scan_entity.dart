80254b03f2368cc6823876e4a3bfb2150a434532
AJAX411 <buitragoramires@gmail.com>
2026-04-29 08:35:00 -0500

    Reorganiza modulo QR por feature

---
+++ lib/features/qr/domain/entities/qr_scan_entity.dart
@@ -0,0 +1,11 @@
+class QrScanEntity {
+  final String rawValue;
+  final String title;
+  final String description;
+
+  const QrScanEntity({
+    required this.rawValue,
+    required this.title,
+    required this.description,
+  });
+}
